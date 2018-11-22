ae9b581596b071e9af9fb17f99852e9ec57d6aa2
Ayanrocks <ayanb1999@gmail.com>
2018-11-22 13:23:24 +0530

    Change token

--- client/src/components/InputForm.vue
+++ client/src/components/InputForm.vue
@@ -9,11 +9,14 @@
             <div class="form-group">
                 <label for="sem">Select semester</label>
                 <select v-model="sem" class="form-control" id="sem">
-                <option value="SM01">Semester 1</option>
+                <option value="SM01" selected>Semester 1</option>
                 <option value="SM02">Semester 2</option>
                 <option value="SM03">Semester 3</option>
-                <option value="SM04" selected>Semester 4</option>
+                <option value="SM04">Semester 4</option>
                 <option value="SM05">Semester 5</option>
+                <option value="SM06">Semester 6</option>
+                <option value="SM07">Semester 7</option>
+                <option value="SM08">Semester 8</option>
                 </select>
             </div>
             <button type="submit" class="btn btn-primary" @click.prevent="formSubmit">Submit</button>
@@ -26,8 +29,8 @@
 export default {
   data() {
     return {
-      roll: "34600116055",
-      sem: "SM04"
+      roll: "",
+      sem: ""
     };
   },
   methods: {
