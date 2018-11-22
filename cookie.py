be36a0da43393a6792b1d45efe4c804576a10da9
Ayanrocks <ayanb1999@gmail.com>
2018-11-22 15:57:33 +0530

    Final release

--- cookie.py
+++ cookie.py
@@ -1,18 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 session = requests.session()
 response = session.get(
     'https://makaut.ucanapply.com/smartexam/public/result-details')
 cookie = session.cookies.get_dict()
-formatted_cookie = "XSRF-TOKEN=" + cookie["XSRF-TOKEN"] + "; examination_session=" + cookie["examination_session"]
-# post = requests.post("http://localhost:5000/get_cookie", data=cookie)
+formatted_cookie = "XSRF-TOKEN=" + \
+    cookie["XSRF-TOKEN"] + "; examination_session=" + \
+    cookie["examination_session"]+";"
 
-html = requests.get("https://makaut.ucanapply.com/smartexam/public/result-details")
-soup = BeautifulSoup(html.content, "lxml")
+soup = BeautifulSoup(response.content, "lxml")
 meta = soup.findAll("meta")
 token = meta[3]["content"]
 print(token)
+
 print(formatted_cookie)
-# post = requests.post("http://localhost:5000/get_token", data=token)
--- index.js
+++ index.js
@@ -18,57 +18,70 @@ app.use(bodyParser.json());
 let cookie = "";
 let token = "";
 
-const processPy = spawn("python3", ["./cookie.py"]);
+// async function runpy() {
+//   console.log("Running PY file");
+//   const processPy = await spawn("python3", ["./cookie.py"]);
+//   processPy.stdout.on("data", data => {
+//     let val = data.toString();
+//     val = val.split("\n");
+//     token = val[0];
+//     cookie = val[1];
+//   });
+// }
 
-function runpy() {
+//Routes
+
+app.post("/result", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  console.log("Running PY file");
+  const processPy = spawn("python3", ["./cookie.py"]);
   processPy.stdout.on("data", data => {
     let val = data.toString();
     val = val.split("\n");
     token = val[0];
     cookie = val[1];
   });
-}
 
-//Routes
+  processPy.on("exit", code => {
+    console.log("process ended with" + code);
+    // console.log(token);
+    // console.log(cookie);
 
-
-app.post("/result", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-
-  request.post(
-    {
-      url: "https://makaut.ucanapply.com/smartexam/public//get-result-details",
-      headers: {
-        "X-CSRF-TOKEN": token,
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        Origin: "https://makaut.ucanapply.com",
-        Cookie: cookie,
-        "X-Requested-With": "XMLHttpRequest",
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
-        Referer: "https://makaut.ucanapply.com/smartexam/public/result-details",
-        Host: "makaut.ucanapply.com"
+    request.post(
+      {
+        url:
+          "https://makaut.ucanapply.com/smartexam/public//get-result-details",
+        headers: {
+          "content-type":
+            "application/x-www-form-urlencoded; charset=UTF-8",
+          Origin: "https://makaut.ucanapply.com",
+          Cookie:cookie,
+          "X-Requested-With": "XMLHttpRequest",
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+          Referer:
+            "https://makaut.ucanapply.com/smartexam/public/result-details",
+          Host: "makaut.ucanapply.com"
+        },
+        form: {
+          _token: token,
+          p1: "",
+          ROLLNO: req.body.roll,
+          SEMCODE: req.body.sem,
+          examtype: "result-details",
+          all: ""
+        }
       },
-      form: {
-        _token: token,
-        p1: "",
-        ROLLNO: req.body.roll,
-        SEMCODE: req.body.sem,
-        examtype: "result-details",
-        all: ""
+      (err, data) => {
+        res.send(data);
       }
-    },
-    (err, data) => {
-      console.log(err);
-      res.send(data);
-    }
-  );
+    );
+  });
 });
 
 app.use(express.static("client/dist"));
 
 app.get("*", (req, res) => {
-  runpy();
   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
