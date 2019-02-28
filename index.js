aae94184fe654d57b14c1a1276d46da39bac2a22
Ayanrocks <ayanb1999@gmail.com>
2019-02-28 21:23:49 +0530

    Increased token fetching efficiency

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("server");
